28da3d7b2c215832d77106ea77322f4c8a0ddfec
kanishk <navalekanishk@gmail.com>
2024-07-28 15:31:21 +0200

    Change: add parametric bspline function

--- kan/network/linear.py
+++ kan/network/linear.py
@@ -1,6 +1,6 @@
 import torch
 
-BASE_ACTIVATION = torch.nn.SiLU
+BASE_ACTIVATION = torch.nn.functional.silu
 
 
 class KANLayer(torch.nn.Module):
@@ -8,16 +8,39 @@ class KANLayer(torch.nn.Module):
         self,
         in_features: int,
         out_features: int,
+        grid_size: int,
+        spline_order: int,
     ) -> None:
         super(KANLayer, self).__init__()
 
         self.in_features = in_features
         self.out_features = out_features
+        self.grid_size = grid_size
+        self.spline_order = spline_order
 
-        self._init_weights()
+        self.wb = self._init_base_weight()
+        self.ws = self._init_spline_weight()
 
-    def _init_weights(self) -> None:
-        self.base = torch.nn.Parameter(
-            torch.Tensor((self.in_features, self.out_features))
+    def _init_base_weight(self) -> torch.nn.Parameter:
+        param = torch.nn.Parameter(
+            torch.randn(size=(self.out_features, self.in_features))
         )
-        torch.nn.init.xavier_normal_(self.base)
+        torch.nn.init.xavier_uniform_(param)
+        return param
+
+    def _init_spline_weight(self) -> torch.nn.Parameter:
+        param = torch.nn.Parameter(
+            torch.ones(
+                size=(
+                    self.out_features,
+                    self.in_features,
+                    self.grid_size + self.spline_order,
+                )
+            )
+        )
+        return param
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        zb = torch.nn.functional.linear(BASE_ACTIVATION(x), self.wb)
+        zs = torch.nn.functional.linear(x, self.ws)
+        return zb + zs
